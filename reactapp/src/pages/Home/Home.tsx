404ce628867f4d244b54044ea43c7c744ddee909
Ronay <94721967+Poeron@users.noreply.github.com>
2024-07-11 15:01:41 +0300

    Refactor Home component to new structure

---
+++ reactapp/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="Home">
+    <h1>Welcome to the Home page</h1>
+    <p>This is the Home page.</p>
+  </div>
+};
+
+export default Home;
